d45d7e375683b20c117ebebecd22b7fd16b9f24a
kritikas726 <kritikas726@gmail.com>
2020-03-23 19:37:46 +0530

    slider

--- js/javascript.js
+++ js/javascript.js
@@ -1,28 +1,50 @@
-banner(){
-    let container=document.getElementsByClassName('banner')[i];
+let i=0;
+console.log(i);
+
+function banner(){
+    let container=document.getElementsByClassName('banner');
+    console.log(i);
+    for (let index = 0; index < container.length; index++) {
+        const element = container[index];
+        element.style.display='none'
+    }
+
     if(i==0){
-        container[1].style.display='none';
-        container[2].style.display='none';
-        container[3].style.display='none';
+        container[0].style.display='flex';
     }
     if(i==1){
-        container[0].style.display='none';
-        container[2].style.display='none';
-        container[3].style.display='none';
+        container[1].style.display='flex';
+      
     }
     if(i==2){
-        container[1].style.display='none';
-        container[0].style.display='none';
-        container[3].style.display='none';
+        container[2].style.display='flex';
     }
     if(i==3){
-        container[1].style.display='none';
-        container[2].style.display='none';
-        container[4].style.display='none';
+        container[3].style.display='flex';
     }
-    if(1==4){
-        i=-1;
+    if(i==4){
+        i=0;
     }
-    i++;
+}
+
+function increment(){
+    i++
+    banner()
+    console.log(i);
+}
+
+function decrement(){
+    i--
+    banner();
+    console.log(i);
 }
     
+
+
+
+
+
+
+
+
+
